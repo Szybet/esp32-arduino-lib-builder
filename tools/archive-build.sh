322978aedb1f8768aacbcbc79a5fcf5158c8c7a8
Szybet <53944559+Szybet@users.noreply.github.com>
2024-06-23 01:51:56 +0200

    pio package

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -11,34 +11,34 @@ rm -rf dist
 mkdir -p dist
 
 cd out
-echo "Creating PlatformIO Tasmota framework-arduinoespressif32"
+echo "Creating PlatformIO framework-arduinoespressif32"
 mkdir -p arduino-esp32/cores/esp32
 mkdir -p arduino-esp32/tools/partitions
 cp -rf ../components/arduino/tools arduino-esp32
 cp -rf ../components/arduino/cores arduino-esp32
 cp -rf ../components/arduino/libraries arduino-esp32
 cp -rf ../components/arduino/variants arduino-esp32
-cp -f ../components/arduino/CMa* arduino-esp32
-cp -f ../components/arduino/idf* arduino-esp32
-cp -f ../components/arduino/Kco* arduino-esp32
-cp -f ../components/arduino/pac* arduino-esp32
+cp -rf ../components/arduino/CMa* arduino-esp32
+cp -rf ../components/arduino/idf* arduino-esp32
+cp -rf ../components/arduino/Kco* arduino-esp32
+cp -rf ../components/arduino/pac* arduino-esp32
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/tests
 rm -rf arduino-esp32/idf_component_examples
-rm -rf arduino-esp32/libraries/RainMaker
-rm -rf arduino-esp32/libraries/Insights
-rm -rf arduino-esp32/libraries/ESP_I2S
-rm -rf arduino-esp32/libraries/SPIFFS
-rm -rf arduino-esp32/libraries/BLE
-rm -rf arduino-esp32/libraries/SimpleBLE
-rm -rf arduino-esp32/libraries/BluetoothSerial
-rm -rf arduino-esp32/libraries/WiFiProv
-rm -rf arduino-esp32/libraries/WiFiClientSecure
-rm -rf arduino-esp32/libraries/NetworkClientSecure
-rm -rf arduino-esp32/libraries/ESP_SR
-rm -rf arduino-esp32/libraries/ESP_NOW
-rm -rf arduino-esp32/libraries/TFLiteMicro
-rm -rf arduino-esp32/libraries/ESP32
+# rm -rf arduino-esp32/libraries/RainMaker
+# rm -rf arduino-esp32/libraries/Insights
+# rm -rf arduino-esp32/libraries/ESP_I2S
+# rm -rf arduino-esp32/libraries/SPIFFS
+# rm -rf arduino-esp32/libraries/BLE
+# rm -rf arduino-esp32/libraries/SimpleBLE
+# rm -rf arduino-esp32/libraries/BluetoothSerial
+# rm -rf arduino-esp32/libraries/WiFiProv
+# rm -rf arduino-esp32/libraries/WiFiClientSecure
+# rm -rf arduino-esp32/libraries/NetworkClientSecure
+# rm -rf arduino-esp32/libraries/ESP_SR
+# rm -rf arduino-esp32/libraries/ESP_NOW
+# rm -rf arduino-esp32/libraries/TFLiteMicro
+# rm -rf arduino-esp32/libraries/ESP32
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/esp32-arduino-libs
 rm -rf arduino-esp32/tools/*.exe
@@ -51,7 +51,7 @@ rm -rf arduino-esp32/programmers.txt
 rm -rf arduino-esp32/boards.txt
 rm -rf arduino-esp32/package.json
 rm -rf arduino-esp32/*.md
-cp -Rf tools/esp32-arduino-libs arduino-esp32/tools/
+cp -rf tools/esp32-arduino-libs arduino-esp32/tools/
 cp ../package.json arduino-esp32/package.json
 cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
 mv arduino-esp32/ framework-arduinoespressif32/
